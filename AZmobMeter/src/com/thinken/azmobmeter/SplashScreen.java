f3bf2ac8532b42bb28580cebedd46dbd2d8a948a
Pedro Minatel <pminatel@gmail.com>
2014-07-20 12:58:08 -0300

    Splash background changed to an static image

--- AZmobMeter/src/com/thinken/azmobmeter/SplashScreen.java
+++ AZmobMeter/src/com/thinken/azmobmeter/SplashScreen.java
@@ -19,7 +19,7 @@ import android.view.View;
  */
 public class SplashScreen extends Activity {
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 4000;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
